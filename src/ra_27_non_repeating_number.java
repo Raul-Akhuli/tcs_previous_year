d58d9e30d590849da4ef47f56bb4cdc5dd8453b9
Raul <akhuliraul@gmail.com>
2025-08-09 21:58:18 +0530

    start from a new pdf tcs 2025 modify no 27 code file

--- src/ra_27_non_repeating_number.java
+++ src/ra_27_non_repeating_number.java
@@ -1,16 +1,26 @@
+/*
+Input:
+9
+
+1 1 2 2 5 6 6 7 7
+
+Output:
+5
+ */
+
 import java.util.Scanner;
 import java.util.Map;
 import java.util.LinkedHashMap;
 public class ra_27_non_repeating_number {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("enter the numbers (space separated): ");
-        String inputLine = sc.nextLine();
+        int n = sc.nextInt();
 
         Map<Character,Integer> freq = new LinkedHashMap<>();
 
-        for(char ch : inputLine.toCharArray()){
+        System.out.println("enter the elements (space separated): ");
+        for(int i = 0; i < n; i++){
+            char ch = sc.next().charAt(0);
             freq.put(ch, freq.getOrDefault(ch, 0) + 1);
         }
 
